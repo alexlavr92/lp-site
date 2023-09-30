06070cb6a9bd85275a8b83b91c4fe4cf8b6b734e
alexlavr92 <lavrinenkoaleksandr@gmail.com>
2023-09-30 03:21:01 +0300

    all edit

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,5 @@
 import '../index.html';
 
-console.log(mult(3, 4));
-console.log(summ(3, 4))
-// summ(3, 4).then((value) => console.log(value));
-
 
 import "scss/style.scss";
 import "scss/pages/index2.scss";
@@ -11,6 +7,6 @@ import "scss/pages/index2.scss";
 
 import './main.js';
 import './main.init.js';
-import { mult, summ } from '../modules/calc';
+
 
 
